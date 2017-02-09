6a67e94cacacda9166d83635f8f9f8ce16b501fc
Alvin Ng <alvindng@gmail.com>
2017-02-09 14:03:23 -0600

    update gitignore file and add firebase credentials

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import { apiKey, authDomain, databaseURL, storageBucket, messagingSenderId } from './credentials';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey,
+    authDomain,
+    databaseURL,
+    storageBucket,
+    messagingSenderId
+    });
+  }
+
   render() {
     return (
       <View>
-        <Header headerText="Authentication"></Header>
+        <Header headerText="Authentication" />
         <Text>Test</Text>
       </View>
     );
-  };
+  }
 }
 
 export default App;
