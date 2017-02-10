e178191ce33f299c468c5d105897a148bbd0cdf4
Alvin Ng <alvindng@gmail.com>
2017-02-10 01:31:04 -0600

    add switch cases to whether a user is logged in or out

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import { apiKey, authDomain, databaseURL, storageBucket, messagingSenderId } from './credentials';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey,
@@ -14,14 +16,36 @@ class App extends Component {
       storageBucket,
       messagingSenderId
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={{ flexDirection: 'row' }}>
+            <Button>Log Out</Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+    }
   }
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <Header headerText="Authentication" />
-
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
